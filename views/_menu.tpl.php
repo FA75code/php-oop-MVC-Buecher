19f05e7dab259e3696ac75267150432b6afad331
tmpMM <33829096+tmpMM@users.noreply.github.com>
2017-11-20 12:37:51 +0100

    added H1 for title based on $header

--- views/_menu.tpl.php
+++ views/_menu.tpl.php
@@ -2,3 +2,5 @@
 <a href="index.php?action=zeige_buch&id=0">buch 1</a>
 <a href="index.php?action=zeige_buch&id=1">buch 2</a>
 <a href="index.php?action=speichere_buch">neues buch</a>
+
+<h1><?php if(isset($header)) {echo $header;} ?></h1>
