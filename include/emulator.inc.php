038b59ceaefed899eca37ba1c9c6c03ed5a6088d
Malte Meyer <1014301@cbw.intern>
2017-11-20 10:32:11 +0100

    cheanged to file storage for book data

--- include/emulator.inc.php
+++ include/emulator.inc.php
@@ -5,18 +5,26 @@
 function hole_buecher()
 {
   $buecher = array();
-  $buecher[] = new Buch(
-    array(
-      'titel' => 'Der Graf von Monte Christo',
-      'preis' => 9.95
-    )
-  );
-  $buecher[] = new Buch(
-    array(
-      'titel' => 'Per Anhalter durch die Galaxis',
-      'preis' => 7.95
-    )
-  );
+  // $buecher[] = new Buch(
+  //   array(
+  //     'titel' => 'Der Graf von Monte Christo',
+  //     'preis' => 9.95
+  //   )
+  // );
+  // $buecher[] = new Buch(
+  //   array(
+  //     'titel' => 'Per Anhalter durch die Galaxis',
+  //     'preis' => 7.95
+  //   )
+  // );
+  // $buecher[] = new Buch(
+  //   array(
+  //     'titel' => 'Cosmos',
+  //     'preis' => 17.95
+  //   )
+  // );
+  // file_put_contents('data/buecher.txt', serialize($buecher));
+  $buecher = unserialize(file_get_contents('data/buecher.txt'));
   return $buecher;
 }
 
